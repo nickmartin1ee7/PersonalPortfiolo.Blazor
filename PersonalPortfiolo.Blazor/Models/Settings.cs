6ad04b32aba5e7215d8a081854aaace7f367fc10
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-07-18 21:07:40 -0400

    Update Nav bar

---
+++ PersonalPortfiolo.Blazor/Models/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalPortfiolo.Blazor.Models;
+
+public class Settings
+{
+    public MainLayoutLink[]? MainLayoutLinks { get; set; }
+}
+
+public class MainLayoutLink
+{
+    public string? Title { get; set; }
+    public string? Uri { get; set; }
+}
