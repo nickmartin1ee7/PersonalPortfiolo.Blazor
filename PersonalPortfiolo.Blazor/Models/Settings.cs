7a4b00316403b1d0889891bcc845df1eedc95700
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-07-18 22:39:30 -0400

    Fix gh repo url using more settings

--- PersonalPortfiolo.Blazor/Models/Settings.cs
+++ PersonalPortfiolo.Blazor/Models/Settings.cs
@@ -3,6 +3,7 @@
 public class Settings
 {
     public bool NavBarEnabled { get; set; }
+    public string? GitHubProfileUri { get; set; }
     public string? GitHubProfileApiUri { get; set; }
     public MainLayoutLink[]? MainLayoutLinks { get; set; }
 }
