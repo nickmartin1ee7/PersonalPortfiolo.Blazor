3ce7ee2d26e68d9d2c1d86f8738423c2b784f83e
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-09-11 01:17:01 -0400

    Update settings model

--- PersonalPortfiolo.Blazor/Models/Settings.cs
+++ PersonalPortfiolo.Blazor/Models/Settings.cs
@@ -5,8 +5,8 @@ public class Settings
     public bool NavBarEnabled { get; set; }
     public string? GitHubProfileUri { get; set; }
     public string? GitHubProfileApiUri { get; set; }
-    public MainLayoutLink[]? MainLayoutLinks { get; set; }
-    public AchievementItem[]? AchievementItems { get; set; }
+    public MainLayoutLink[] MainLayoutLinks { get; set; } = Array.Empty<MainLayoutLink>();
+    public AchievementItem[] AchievementItems { get; set; } = Array.Empty<AchievementItem>();
 }
 
 public class MainLayoutLink
@@ -23,5 +23,5 @@ public class AchievementItem
     public string? Description { get; set; }
     public string? UriDescription { get; set; }
     public string? Uri { get; set; }
-    public string[]? Bullets { get; set; }
+    public string[] Bullets { get; set; } = Array.Empty<string>();
 }
